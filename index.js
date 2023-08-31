874a06fd0189345687a147104d59d6ea559bcee9
Ben Thompson <ben_thompson12@hotmail.com>
2023-08-31 18:53:45 +0100

    working image shifts, before attempting to fix gradient in chrome

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const projects = document.getElementsByClassName("project");
 
 Array.from(projects).forEach((project) => {
+  const x = project.children[0];
+  const image = project.children[1];
 
-  const image = project.children[0];
-  
+  // Function to move images on hover
+
   const handleMove = (event) => {
-    
     let dimensions = project.getBoundingClientRect();
 
     const height = dimensions.height;
     const width = dimensions.width;
-    
+
     // Get position of cursor on mouseover
     const x = event.clientX;
     const y = event.clientY;
@@ -18,26 +19,32 @@ Array.from(projects).forEach((project) => {
     // Calculate position of cursor relative to the project, in pixels
     const localX = x - dimensions.left;
     const localY = y - dimensions.top;
-    
+
     // Calculate rotation value for x and y axes (value between -1 and 1)
     const xRotation = 2 * ((localX - height / 2) / height);
     const yRotation = 2 * ((localY - width / 2) / width);
-    
-    image.style.setProperty('--mouse-x', String(xRotation));
-    image.style.setProperty('--mouse-y', String(yRotation));
+
+    image.style.setProperty("--mouse-x", String(xRotation));
+    image.style.setProperty("--mouse-y", String(yRotation));
+  };
+
+  // When project is clicked, add selected class and remove click listener
+
+  const projectClick = (event) => {
+    project.classList.add("selected");
+    project.removeEventListener("click", projectClick);
+  };
+
+  // When x is clicked, remove selected class, and re-add click listener after timeout
+
+  const xClick = (event) => {
+    project.classList.remove("selected");
+    setTimeout(() => {
+      project.addEventListener("click", projectClick);
+    }, 100);
   };
 
   project.addEventListener("mousemove", handleMove);
-
+  project.addEventListener("click", projectClick);
+  x.addEventListener("click", xClick);
 });
-
-Array.from(projects).forEach(project => {
-
-    const handleClick = event => {
-        project
-    }
-
-
-
-    project.addEventListener('click', handleClick);
-})
